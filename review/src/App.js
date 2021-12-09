57594a61c1db4ccca483c635b862b3678532f5cd
Warren C. Longmire <warren.longmire@lambdaschool.com>
2021-12-09 12:40:59 -0500

    lecture work-in-progress

--- review/src/App.js
+++ review/src/App.js
@@ -13,23 +13,23 @@ const App = ()=> {
 const SubComp1 = (props) => {
     const { person } = props;
 
-    console.log(person);
-    
     return(<div className="component">
         <h2>SubComp1</h2>
         <p>{`${person.name.title} ${person.name.first} ${person.name.last}`}
-        <SubComp2/> </p>
+        <SubComp2 person={person}/> </p>
     </div>)
 }
 
-const SubComp2 = () => {
+const SubComp2 = (props) => {
+    const { person } = props;
     return(<div className="component">
         <h2>SubComp2</h2>
-        <SubComp3/>
+        <SubComp3 person={person}/>
     </div>)
 }
 
-const SubComp3 = ()=> {
+const SubComp3 = (props)=> {
+    const { person } = props;
     return(<div className="component">
         <h2>SubComp3</h2>
     </div>);
