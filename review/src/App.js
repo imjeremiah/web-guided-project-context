c5301b623402de864c779e32dd15d358bc8d2f3d
Warren C. Longmire <warren.longmire@lambdaschool.com>
2021-12-09 12:37:07 -0500

    lecture work-in-progress

--- review/src/App.js
+++ review/src/App.js
@@ -2,6 +2,8 @@ import React, { useState } from 'react';
 import data from '..data';
 
 const App = ()=> {
+    const [person, setPerson] = useState(data);
+    
     return(<div className="App component">
         <h1>Main App</h1>
         <SubComp1/>
