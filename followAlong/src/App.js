c2317fc9ce1931c3337eb67bfcbb462ecf1d03ca
Warren C. Longmire <warren.longmire@lambdaschool.com>
2021-12-09 13:31:56 -0500

    lecture work-in-progress

--- followAlong/src/App.js
+++ followAlong/src/App.js
@@ -26,7 +26,7 @@ export default function App() {
           </button>
         ))}
       </section>
-      <FamilyContext.Provider value={activeFamily}}>
+      <FamilyContext.Provider value={activeFamily}>
         {activeFamily && <FamilyTree />}
       </FamilyContext.Provider>
     </div>
