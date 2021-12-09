90889cc0adf958d1ff5c498709d68ddc7fcad51b
Warren C. Longmire <warren.longmire@lambdaschool.com>
2021-12-09 13:06:24 -0500

    lecture work-in-progress

---
+++ review/src/AppContextReducer.js
@@ -0,0 +1,60 @@
+import React, { useReducer, createContext, useContext } from 'react';
+import  { reducer, initialState} from './reducer';
+import { setName, setLocation }  from './reducer';
+
+const PersonContext = createContext();
+
+const App = ()=> {
+    const [person, dispatch] = useReducer(reducer, initialState);
+
+    return(
+        <div className="App component">
+            <h1>Main App</h1>
+            <PersonContext.Provider value={{person, dispatch}}>
+                <SubComp1/>
+            </PersonContext.Provider>
+        </div>);
+};
+
+const SubComp1 = () => {
+    const { person } = useContext(PersonContext);
+
+    return(<div className="component">
+        <h2>SubComp1</h2>
+        <p>{`${person.name.title} ${person.name.first} ${person.name.last}`}</p>
+        <SubComp2/>
+    </div>)
+}
+
+const SubComp2 = () => {
+    const {dispatch} = useContext(PersonContext);
+
+    const handleClick = ()=> {
+        dispatch(setLocation({
+            street: "222 N 22nd Street",
+            city: "Philadelphia",
+            state: "PA"
+        }));
+    }
+
+    return(<div className="component">
+        <h2>SubComp2</h2>
+        <button onClick={handleClick}>Change our location</button>
+        <SubComp3 />
+    </div>)
+}
+
+const SubComp3 = ()=> {
+    const { person, dispatch } = useContext(PersonContext);
+
+    const handleClick = ()=> {
+        dispatch(setName({title: "Mx", first: "First", last: "Last" }))
+    }
+
+    return(<div className="component">
+        <h2>SubComp3</h2>
+        <button onClick={handleClick}>Change our Name</button>
+        <p>{`${person.location.street} ${person.location.city}, ${person.location.state}`}</p>
+    </div>);
+}
+export default App;
