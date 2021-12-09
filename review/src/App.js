f935f3cd78395a4869d2df721a817201ec45bc00
Warren C. Longmire <warren.longmire@lambdaschool.com>
2021-12-09 12:34:43 -0500

    lecture work-in-progress

--- review/src/App.js
+++ review/src/App.js
@@ -3,7 +3,14 @@ import React from 'react';
 const App = ()=> {
     return(<div className="App component">
         <h1>Main App</h1>
+        <SubComp1/>
     </div>);
 };
+
+const SubComp1 = () => {
+    return(<div className="component">
+        <h2>SubComp1</h2>
+    </div>)
+}
 
 export default App;
